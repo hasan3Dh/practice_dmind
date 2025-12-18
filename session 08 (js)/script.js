1bb71637e0a3d77e32dca8bcf052add9fea27e8a
Hasan Hashemian <33954179+hasan3Dh@users.noreply.github.com>
2025-12-18 16:55:17 +0330

    Add files via upload

---
+++ session 08 (js)/script.js
@@ -0,0 +1,304 @@
+var number1 = 18;
+var number2 = 19;
+var number3 = 12;
+var number4 = 17;
+var number5 = 19;
+var number6 = 20;
+var number7 = 13;
+var number8 = 14;
+var number9 = 17;
+var number10 = 16;
+
+var x = number1 +
+        number2 + 
+        number3 + 
+        number4 + 
+        number5 + 
+        number6 + 
+        number7 + 
+        number8 + 
+        number9 + 
+        number10;
+
+x = x / 10 ;
+console.log("avrage :", x);
+var variance =
+    (number1 - x) ** 2 +
+    (number2 - x) ** 2 + 
+    (number3 - x) ** 2 +
+    (number4 - x) ** 2 +
+    (number5 - x) ** 2 +
+    (number6 - x) ** 2 +
+    (number7 - x) ** 2 +
+    (number8 - x) ** 2 +
+    (number9 - x) ** 2 +
+    (number10 - x) ** 2;
+
+variance = variance / 10 ;
+console.log("Variance : ",variance);
+
+// maximum number
+if(
+    number1 >= number2 &&
+    number1 >= number3 &&
+    number1 >= number4 &&
+    number1 >= number5 &&
+    number1 >= number6 &&
+    number1 >= number7 &&
+    number1 >= number8 &&
+    number1 >= number9 &&
+    number1 >= number10
+){
+    console.log("Maximum Number :",number1);
+}
+else if(
+    number2 >= number1 &&
+    number2 >= number3 &&
+    number2 >= number4 &&
+    number2 >= number5 &&
+    number2 >= number6 &&
+    number2 >= number7 &&
+    number2 >= number8 &&
+    number2 >= number9 &&
+    number2 >= number10
+){
+    console.log("Maximum Number :",number2);
+}
+else if(
+    number3 >= number1 &&
+    number3 >= number2 &&
+    number3 >= number4 &&
+    number3 >= number5 &&
+    number3 >= number6 &&
+    number3 >= number7 &&
+    number3 >= number8 &&
+    number3 >= number9 &&
+    number3 >= number10
+){
+    console.log("Maximum Number :",number3);
+}
+else if(
+    number4 >= number1 &&
+    number4 >= number2 &&
+    number4 >= number3 &&
+    number4 >= number5 &&
+    number4 >= number6 &&
+    number4 >= number7 &&
+    number4 >= number8 &&
+    number4 >= number9 &&
+    number4 >= number10
+){
+    console.log("Maximum Number :",number4);
+}
+else if(
+    number5 >= number1 &&
+    number5 >= number2 &&
+    number5 >= number3 &&
+    number5 >= number4 &&
+    number5 >= number6 &&
+    number5 >= number7 &&
+    number5 >= number8 &&
+    number5 >= number9 &&
+    number5 >= number10
+){
+    console.log("Maximum Number :",number5);
+}
+else if(
+    number6 >= number1 &&
+    number6 >= number2 &&
+    number6 >= number3 &&
+    number6 >= number4 &&
+    number6 >= number5 &&
+    number6 >= number7 &&
+    number6 >= number8 &&
+    number6 >= number9 &&
+    number6 >= number10
+){
+    console.log("Maximum Number :",number6);
+}
+else if(
+    number7 >= number1 &&
+    number7 >= number2 &&
+    number7 >= number3 &&
+    number7 >= number4 &&
+    number7 >= number5 &&
+    number7 >= number6 &&
+    number7 >= number8 &&
+    number7 >= number9 &&
+    number7 >= number10
+){
+    console.log("Maximum Number :",number7);
+}
+else if(
+    number8 >= number1 &&
+    number8 >= number2 &&
+    number8 >= number3 &&
+    number8 >= number4 &&
+    number8 >= number5 &&
+    number8 >= number6 &&
+    number8 >= number7 &&
+    number8 >= number9 &&
+    number8 >= number10
+){
+    console.log("Maximum Number :",number8);
+}
+else if(
+    number9 >= number1 &&
+    number9 >= number2 &&
+    number9 >= number3 &&
+    number9 >= number4 &&
+    number9 >= number5 &&
+    number9 >= number6 &&
+    number9 >= number7 &&
+    number9 >= number8 &&
+    number9 >= number10
+){
+    console.log("Maximum Number :",number9);
+}
+else if(
+    number10 >= number1 &&
+    number10 >= number2 &&
+    number10 >= number3 &&
+    number10 >= number4 &&
+    number10 >= number5 &&
+    number10 >= number6 &&
+    number10 >= number7 &&
+    number10 >= number8 &&
+    number10 >= number9
+){
+    console.log("Maximum Number :",number10);
+}
+
+
+
+// minimum number
+if(
+    number1 <= number2 &&
+    number1 <= number3 &&
+    number1 <= number4 &&
+    number1 <= number5 &&
+    number1 <= number6 &&
+    number1 <= number7 &&
+    number1 <= number8 &&
+    number1 <= number9 &&
+    number1 <= number10
+){
+    console.log("Minimum Number :",number1);
+}
+else if(
+    number2 <= number1 &&
+    number2 <= number3 &&
+    number2 <= number4 &&
+    number2 <= number5 &&
+    number2 <= number6 &&
+    number2 <= number7 &&
+    number2 <= number8 &&
+    number2 <= number9 &&
+    number2 <= number10
+){
+    console.log("Minimum Number :",number2);
+}
+else if(
+    number3 <= number1 &&
+    number3 <= number2 &&
+    number3 <= number4 &&
+    number3 <= number5 &&
+    number3 <= number6 &&
+    number3 <= number7 &&
+    number3 <= number8 &&
+    number3 <= number9 &&
+    number3 <= number10
+){
+    console.log("Minimum Number :",number3);
+}
+else if(
+    number4 <= number1 &&
+    number4 <= number2 &&
+    number4 <= number3 &&
+    number4 <= number5 &&
+    number4 <= number6 &&
+    number4 <= number7 &&
+    number4 <= number8 &&
+    number4 <= number9 &&
+    number4 <= number10
+){
+    console.log("Minimum Number :",number4);
+}
+else if(
+    number5 <= number1 &&
+    number5 <= number2 &&
+    number5 <= number3 &&
+    number5 <= number4 &&
+    number5 <= number6 &&
+    number5 <= number7 &&
+    number5 <= number8 &&
+    number5 <= number9 &&
+    number5 <= number10
+){
+    console.log("Minimum Number :",number5);
+}
+else if(
+    number6 <= number1 &&
+    number6 <= number2 &&
+    number6 <= number3 &&
+    number6 <= number4 &&
+    number6 <= number5 &&
+    number6 <= number7 &&
+    number6 <= number8 &&
+    number6 <= number9 &&
+    number6 <= number10
+){
+    console.log("Minimum Number :",number6);
+}
+else if(
+    number7 <= number1 &&
+    number7 <= number2 &&
+    number7 <= number3 &&
+    number7 <= number4 &&
+    number7 <= number5 &&
+    number7 <= number6 &&
+    number7 <= number8 &&
+    number7 <= number9 &&
+    number7 <= number10
+){
+    console.log("Minimum Number :",number7);
+}
+else if(
+    number8 <= number1 &&
+    number8 <= number2 &&
+    number8 <= number3 &&
+    number8 <= number4 &&
+    number8 <= number5 &&
+    number8 <= number6 &&
+    number8 <= number7 &&
+    number8 <= number9 &&
+    number8 <= number10
+){
+    console.log("Minimum Number :",number8);
+}
+else if(
+    number9 <= number1 &&
+    number9 <= number2 &&
+    number9 <= number3 &&
+    number9 <= number4 &&
+    number9 <= number5 &&
+    number9 <= number6 &&
+    number9 <= number7 &&
+    number9 <= number8 &&
+    number9 <= number10
+){
+    console.log("Minimum Number :",number9);
+}
+else if(
+    number10 <= number1 &&
+    number10 <= number2 &&
+    number10 <= number3 &&
+    number10 <= number4 &&
+    number10 <= number5 &&
+    number10 <= number6 &&
+    number10 <= number7 &&
+    number10 <= number8 &&
+    number10 <= number9
+){
+    console.log("Minimum Number :",number10);
+}
